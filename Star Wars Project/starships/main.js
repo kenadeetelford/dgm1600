7e3bc5b8e9c374eaeef8bd45db657f27f5f083f0
kenadeetelford <kenadee.telford@gmail.com>
2020-03-17 17:48:06 -0600

    starships

--- Star Wars Project/starships/main.js
+++ Star Wars Project/starships/main.js
@@ -7,12 +7,14 @@ const navList = document.querySelector('.navList')
 function populateNav(starships) {
     starships.forEach(starship => {
         letshipAnchor = document.createElement('a')
+        shipAnchor.href = '#'
         let listItem = document.createElement ('li')
         listItem.textContent = starship.name
     
    shipAnchor.appendChild(listItem) 
    navList.appendChild(shipAnchor)
-    })
+   
+})
 nav.appendChild(navList)
 }
 
