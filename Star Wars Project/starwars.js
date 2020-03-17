968a265b74ce6d83a0699faf482822639f30f3fb
kenadeetelford <kenadee.telford@gmail.com>
2020-03-17 14:43:07 -0600

    pictures

--- Star Wars Project/starwars.js
+++ Star Wars Project/starwars.js
@@ -2,24 +2,36 @@ import { films } from '../data/films.js'
 import { people } from '../data/people.js'
 
 console.log("Hi there! My first Javascript code!")
-console.log(document.querySelector(".greeting"))
-let greetingDiv = document.querySelector (".greeting")
 
-greetingDiv.textContent = "Star Wars Cast" 
+console.log(document.querySelector('.greeting'))
+let greetingDiv = document.querySelector ('.greeting')
 
 let castList = document.createElement("ul")
 
 let counter = 1
 
-people.forEach((person) => {
+people.forEach(person => {
 let nameItem = document.createElement("li")
 nameItem.textContent = person.name
 castList.appendChild(nameItem)
+
+let personAnchor = document.createElement ("a")
+personAnchor.href = "#"
 let personImg = document.createElement("img")
 personImg.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
-greetingDiv.appendChild(personImg)
+
+personImg.addEventListener('error', (event) => {
+    personImg.hidden = true 
+    //personImg.src = '../images/uvu.jpeg'
+})
+personImg.addEventListener("click", function( event) {
+    console.log('Thanks for clicking!')
+})
+personAnchor.appendChild(personImg)
+greetingDiv.appendChild(personAnchor)
 counter++ 
 })
+
 
 
 greetingDiv.appendChild(castList)
