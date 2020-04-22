dbc4a7ab6f11467ac4794bdb09253af932a86570
kenadeetelford <kenadee.telford@gmail.com>
2020-04-21 18:33:58 -0600

    improved getAPIData function using async/await

--- Pokemon/index.js
+++ Pokemon/index.js
@@ -1,7 +1,7 @@
 
 let pokeContainer = document.querySelector('.pokeContainer')
 
-function getPokeData(url) {
+/* function getPokeData(url) {
  fetch(url).then(function (response) {
     response.json().then(function (pokemon) {
         console.log(pokemon.results)
@@ -10,28 +10,50 @@ function getPokeData(url) {
   })
 }
 
-getPokeData("https://pokeapi.co/api/v2/pokemon")
+getPokeData("https://pokeapi.co/api/v2/pokemon")*/
 // https://pokeapi.co/api/v2/pokemon/1/
 
-function populatePokeCards(pokeArray) {
-    pokeArray.forEach((pokemon) => {
-    let pokeScene = document.createElement('div')
-    pokeScene.className = 'scene'
-    let pokeCard = document.createElement('div')
-    pokeCard.className = 'card'
-    pokeCard.addEventListener( 'click', function() {
-        pokeCard.classList.toggle('is-flipped');
-      });
-    let pokeFront = document.createElement('div')
-    pokeFront.className = 'card__face card__face--front'
-    pokeFront.textContent = "Front!"
-    let pokeBack = document.createElement('div')
-    pokeBack.className = 'card__face card__face--back'
-    pokeBack.textContent = "Back!"
+async function getAPIData(url) {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+    }
+    catch (error) {
+        console.error(error)
+    }
+}
 
-    pokeCard.appendChild(pokeFront)
-    pokeCard.appendChild(pokeBack)
-    pokeScene.appendChild(pokeCard)
-    pokeContainer.appendChild(pokeScene)
-    })
+getAPIData('https://pokeapi.co/api/v2/pokemon/?&limit=25').then(
+    (data) => {
+        for (const pokemon of data.results) {
+            getAPIData(pokemon.url).then(
+                (pokeData) => {
+                    populatePokeCard(pokeData)
+                }
+            )
+        }
+
+    }
+)
+
+function populatePokeCard(singlePokemon) {
+        let pokeScene = document.createElement('div')
+        pokeScene.className = 'scene'
+        let pokeCard = document.createElement('div')
+        pokeCard.className = 'card'
+        pokeCard.addEventListener('click', function () {
+            pokeCard.classList.toggle('is-flipped');
+        })
+        let pokeFront = document.createElement('div')
+        pokeFront.className = 'card__face card__face--front'
+        pokeFront.textContent = singlePokemon.name
+        let pokeBack = document.createElement('div')
+        pokeBack.className = 'card__face card__face--back'
+        pokeBack.textContent = "Back!"
+
+        pokeCard.appendChild(pokeFront)
+        pokeCard.appendChild(pokeBack)
+        pokeScene.appendChild(pokeCard)
+        pokeContainer.appendChild(pokeScene)
 }
