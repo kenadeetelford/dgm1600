32b40e2ebc0bbb9015009c9c92bec21c47c53e65
kenadeetelford <kenadee.telford@gmail.com>
2020-04-21 18:55:04 -0600

    added images and refactored populate functions

--- Pokemon/index.js
+++ Pokemon/index.js
@@ -45,15 +45,24 @@ function populatePokeCard(singlePokemon) {
         pokeCard.addEventListener('click', function () {
             pokeCard.classList.toggle('is-flipped');
         })
-        let pokeFront = document.createElement('div')
-        pokeFront.className = 'card__face card__face--front'
-        pokeFront.textContent = singlePokemon.name
-        let pokeBack = document.createElement('div')
-        pokeBack.className = 'card__face card__face--back'
-        pokeBack.textContent = "Back!"
-
+        let pokeFront = populateCardFront(singlePokemon)
+        let pokeBack = populateCardBack(singlePokemon)
+       
         pokeCard.appendChild(pokeFront)
         pokeCard.appendChild(pokeBack)
         pokeScene.appendChild(pokeCard)
         pokeContainer.appendChild(pokeScene)
+}
+
+function populateCardFront(pokeMon) {
+    let cardFront = document.createElement('div')
+        cardFront.className = 'card__face card__face--front'
+        cardFront.textContent = PokeMon.name
+        return cardFront
+}
+function populateCardFront(pokeMon) {
+    let cardBack = document.createElement('div')
+        cardBack.className = 'card__face card__face--back'
+        cardBack.textContent = 'Back!'
+        return cardBack
 }
