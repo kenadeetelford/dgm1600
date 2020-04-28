c28a2e51fde341644e91c0d26d38534ceb2cd211
kenadeetelford <kenadee.telford@gmail.com>
2020-04-28 05:41:50 -0600

    fixing starships

--- utils.js
+++ utils.js
@@ -11,3 +11,17 @@ export function getLastNumber(url) {
     }
     return url.slice(start,end)
 }
+
+export function addStarField(element, numStars) {
+    element.style.setProperty('background-color', 'black')
+    for (let i = 0; i < numStars; i++) {
+      let star = document.createElement('div')
+      star.style.setProperty('position', 'absolute')
+      star.style.setProperty('width', '1px')
+      star.style.setProperty('height', '1px')
+      let xy = getRandomPosition()
+      star.style.left = `${xy[0]}px`
+      star.style.top = `${xy[1]}px`
+      element.appendChild(star)
+    }
+  }
