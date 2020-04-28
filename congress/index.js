0ed21c69065469c1f08ae9cee4eb4bba5f1e3fb8
kenadeetelford <kenadee.telford@gmail.com>
2020-04-28 03:15:36 -0600

    java

--- congress/index.js
+++ congress/index.js
@@ -4,10 +4,10 @@ console.log(senators.length)
 
 const container = document.querySelector('.container')
 
-const filterSenators = (prop, value) => {
+const filterSenators = (prop,value) => {
     return senators.filter(senator => senator[prop] === value)
 }
 
 console.log(filterSenators('party','R'))
 console.log(filterSenators('party', 'D'))
-console.log(filterSenators)('party','ID')
+console.log(filterSenators('party','ID'))
